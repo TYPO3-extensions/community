7bd725db77761b64c65be3130397dd0896c3fd08
flyguide <flyguide@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-08-07 08:15:51 +0000

    initial code generated with kickstarter
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/community/trunk@11000 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "community"
+#
+# Auto generated 07-08-2008 10:10
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Community',
+	'description' => 'A flexible and extensible community system.',
+	'category' => 'plugin',
+	'author' => 'Ingo Renner',
+	'author_email' => 'ingo@typo3.org',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:7:{s:9:"ChangeLog";s:4:"1ffb";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"c0f8";s:19:"doc/wizard_form.dat";s:4:"23fa";s:20:"doc/wizard_form.html";s:4:"8344";s:30:"static/community/constants.txt";s:4:"d41d";s:26:"static/community/setup.txt";s:4:"d41d";}',
+);
+
+?>
