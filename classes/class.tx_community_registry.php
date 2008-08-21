f9d10fc1e51c336e9eea1b397dd219ed179e9271
neoblack <neoblack@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-08-21 22:11:03 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/community/trunk@11328 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ classes/class.tx_community_registry.php
@@ -0,0 +1,91 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2008 Frank Nägler <typo3@naegler.net>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+
+/**
+ * registry class to store objects and make them available across the layers
+ * of the MVC Pattern
+ *
+ * @author	Frank Nägler <typo3@naegler.net>
+ * @package TYPO3
+ * @subpackage community
+ */
+class tx_community_Registry {
+
+	protected static $instances = array();
+	protected $extKey;
+	protected $configuration = null;
+
+	/**
+	 * singleton instance access method for the Registry class
+	 *
+	 * @return tx_community_Registry
+	 */
+	public function getInstance($extKey = 'tx_community') {
+		if (!isset(self::$instances[$extKey])) {
+			self::$instances[$extKey] = new tx_community_Registry($extKey);
+		}
+		return self::$instances[$extKey];
+	}
+
+	/**
+	 * constructor for class tx_community_Registry
+	 *
+	 */
+	protected function __construct($extKey) {
+		$this->extKey = $extKey;
+	}
+
+	/**
+	 * clone interceptor method, declared private to implement the singleton pattern
+	 *
+	 */
+	private function __clone() {}
+
+	/**
+	 * sets the configuration arrray in the registry
+	 *
+	 * @param	array	configuration array to store
+	 */
+	public function setConfiguration(array $configuration) {
+		$this->configuration = $configuration;
+	}
+
+	/**
+	 * gets the configuration array stored in the registry. 
+	 * returns null if no configuration is available for the given key
+	 *
+	 * @return	mixed
+	 */
+	public function getConfiguration() {
+		return $this->configuration;
+	}
+}
+
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/community/classes/class.tx_community_Registry.php'])	{
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/community/classes/class.tx_community_Registry.php']);
+}
+
+?>
