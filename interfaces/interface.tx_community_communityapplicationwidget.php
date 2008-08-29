24e25f110e20441a255134ea84757767c18a38ff
neoblack <neoblack@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-08-29 12:03:24 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/community/trunk@11532 735d13b6-9817-0410-8766-e36946ffe9aa

--- interfaces/interface.tx_community_communityapplicationwidget.php
+++ interfaces/interface.tx_community_communityapplicationwidget.php
@@ -39,8 +39,6 @@ interface tx_community_CommunityApplicationWidget {
 
 	public function setParentCommunityApplication(tx_community_controller_AbstractCommunityApplication $parentCommunityApplication);
 
-	public function execute();
-	
 	/**
 	 * returns whether a user is allowed to drag the widget to a different
 	 * container or position
