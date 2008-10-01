5776d000280e79c7209a3d246774929be819c87a
neoblack <neoblack@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-10-01 14:35:03 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/community/trunk@12554 735d13b6-9817-0410-8766-e36946ffe9aa

--- view/listgroups/class.tx_community_view_listgroups_index.php
+++ view/listgroups/class.tx_community_view_listgroups_index.php
@@ -75,6 +75,7 @@ class tx_community_view_listGroups_Index extends tx_community_view_AbstractView 
 			)
 		);
 
+		$loopGroups = array();
 		foreach ($this->groups as $group) {
 			$tmp = array(
 				'uid'         => $group->getUid(),
