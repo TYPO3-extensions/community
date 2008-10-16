a8bc157d170ec7ef4f974cfd79eb3ac7eed23916
neoblack <neoblack@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-10-16 21:32:29 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/community/trunk@13055 735d13b6-9817-0410-8766-e36946ffe9aa

--- controller/userprofile/class.tx_community_controller_userprofile_profileactionswidget.php
+++ controller/userprofile/class.tx_community_controller_userprofile_profileactionswidget.php
@@ -36,7 +36,7 @@ class tx_community_controller_userprofile_ProfileActionsWidget extends tx_commun
 
 	public function __construct() {
 		parent::__construct();
-		$this->localizationManager = tx_community_LocalizationManager::getInstance('EXT:community/lang/locallang_userprofile_personalinformation.xml', $GLOBALS['TSFE']->tmpl->setup['plugin.']['tx_community.']);
+		$this->localizationManager = tx_community_LocalizationManager::getInstance('EXT:community/lang/locallang_userprofile_profileactions.xml', $GLOBALS['TSFE']->tmpl->setup['plugin.']['tx_community.']);
 		
 		$this->name     	= 'profileActions';
 		$this->label    	= $this->localizationManager->getLL('label_ProfileActionWidget');
