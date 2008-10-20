48371e4f76fe362f04c50c074cfab3e0219b21bd
flyguide <flyguide@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-10-20 11:31:04 +0000

    changed method getResourceId in the abstract community application controller, the check for a null user is not necessary as there's always a user
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/community/trunk@13168 735d13b6-9817-0410-8766-e36946ffe9aa

--- controller/class.tx_community_controller_abstractcommunityapplication.php
+++ controller/class.tx_community_controller_abstractcommunityapplication.php
@@ -251,17 +251,16 @@ abstract class tx_community_controller_AbstractCommunityApplication extends tsli
 	 * @return string
 	 */
 	public function getResourceId() {
-		$resourceId = '';
-		
+		$resourceId    = '';
 		$requestedUser = $this->getRequestedUser();
-		if (!is_null($requestedUser)) {
-			$resourceId = $this->name
-				. '_' . $this->accessMode
-				. '_' . $requestedUser->getUid();
-		}		
+
+		$resourceId = $this->name
+			. '_' . $this->accessMode
+			. '_' . $requestedUser->getUid();
+
 		return $resourceId;
 	}
-	
+
 	/**
 	 * gets the user gateway
 	 *
