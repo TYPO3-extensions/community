6c8bb44f7f42d4305f50bd542a139549b83d4f4d
neoblack <neoblack@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-11-05 16:43:39 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/community/trunk@13779 735d13b6-9817-0410-8766-e36946ffe9aa

--- classes/viewhelper/class.tx_community_viewhelper_date.php
+++ classes/viewhelper/class.tx_community_viewhelper_date.php
@@ -52,6 +52,7 @@ class tx_community_viewhelper_Date implements tx_community_ViewHelper {
 
 	public function execute(array $arguments = array()) {
 		if ($arguments[1]) {
+			$this->dateFormat = $GLOBALS['TSFE']->tmpl->setup['plugin.']['tx_community.']['general.']['dateFormat.'];
 			$this->dateFormat['date'] = $arguments[1];
 		}
 		return $this->contentObject->stdWrap($arguments[0], $this->dateFormat);
