b3bb7feafa7b196d36410e517044845103c7c906
neoblack <neoblack@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-01-22 16:37:22 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/community/trunk@15974 735d13b6-9817-0410-8766-e36946ffe9aa

--- controller/class.tx_community_controller_listgroupsapplication.php
+++ controller/class.tx_community_controller_listgroupsapplication.php
@@ -179,6 +179,7 @@ class tx_community_controller_ListGroupsApplication extends tx_community_control
 		
 		$pageBrowserConfig = $this->configuration['applications.']['listGroups.']['pageBrowser.'];
 		$pageBrowserConfig['numberOfPages'] = ceil(count($listGroupsArray) / $pageBrowserConfig['numberOfEntriesPerPage']);
+		$pageBrowserConfig['extraQueryString'] = '&tx_community[action]=search&tx_community[quicksearch]=' . $searchValue;
 		$firstGroup = (isset($this->request['page'])) ? (intval($this->request['page']+1)*$pageBrowserConfig['numberOfEntriesPerPage']) - $pageBrowserConfig['numberOfEntriesPerPage'] + 1 : 1; 
 		
 		$tmp = array();
